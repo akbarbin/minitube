34307ea8554709032d00ad67378cae0b598b4451
Muhamad Akbar Bin Widayat <muhamadakbarbw@gmail.com>
2018-08-09 10:01:29 +0800

    Initialize Vue Js

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'hello#index'
+
+  get 'hello/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
